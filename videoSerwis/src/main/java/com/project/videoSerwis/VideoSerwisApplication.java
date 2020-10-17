12aa8394f1ccb6f9ccf34238d1744316cd235f71
PKulesza-iswireless <p.kulesza@is-wireless.com>
2020-10-17 17:06:52 +0200

    first commit

---
+++ videoSerwis/src/main/java/com/project/videoSerwis/VideoSerwisApplication.java
@@ -0,0 +1,13 @@
+package com.project.videoSerwis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSerwisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSerwisApplication.class, args);
+	}
+
+}
---
+++ videoSerwis/src/test/java/com/project/videoSerwis/VideoSerwisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.videoSerwis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSerwisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
